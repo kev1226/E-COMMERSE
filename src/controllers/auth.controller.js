af2a2b3ef1b922c16fbeb6562ef173ffaa2cd398
kev1226 <kevin_alt12@hotmail.com>
2025-01-27 14:23:25 -0500

    contrasenia con bcrypt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,27 @@
 import User from "../models/user.model.js"
+import bcrypt from 'bcryptjs'
+
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body
    try {
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const newUser = new User({
         username,
         email,
-        password
+        password: passwordHash,
     });
 
-    const userSave = await newUser.save();
-    res.json(userSave)
+    const userSaved = await newUser.save();
+    res.json({
+        id: userSaved._id,
+        username: userSaved.username,
+        email: userSaved.email,
+        createdAT: userSaved.createdAt,
+        updatedAt: userSaved.updatedAt,
+    })
 
    } catch (error) {
     console.log("Error saving")
