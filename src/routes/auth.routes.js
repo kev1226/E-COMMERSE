44a9eebf26e9eab88485929f3c1ac36b81dd8ceb
kev1226 <kevin_alt12@hotmail.com>
2025-02-02 22:37:21 -0500

    update commits

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 // Para crear un enrutador
 import { Router } from "express";
-import { login,register } from "../controllers/auth.controller.js";
+import { login,register, logout } from "../controllers/auth.controller.js";
 // Objeto para crear multiples rutas (peticiosnes get, post...)
 
 const router = Router()
 
 router.post('/register', register)
 router.post('/login', login)
+router.post('/logout', logout)
 
 export default router
