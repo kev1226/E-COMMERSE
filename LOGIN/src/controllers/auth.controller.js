918fc0ee9b15e34ccd845a7c5e0850b465e733f1
kev1226 <kevin_alt12@hotmail.com>
2025-02-03 21:58:14 -0500

    Validate token

---
+++ LOGIN/src/controllers/auth.controller.js
@@ -0,0 +1,91 @@
+import User from "../models/user.model.js"
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from "../libs/jwt.js";
+
+export const register = async (req, res) => {
+    const { email, password, username } = req.body
+    try {
+        // encriptando contraseña
+        const passwordHash = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            username,
+            email,
+            password: passwordHash,
+        });
+
+        // usando token
+        const userSaved = await newUser.save();
+        const token = await createAccessToken({ id: userSaved._id })
+
+        res.cookie("token", token)
+
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAT: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+
+        const UserFound = await User.findOne({ email })
+        if (!UserFound) return res.status(400).json({
+            message:
+                "User not fount"
+        })
+
+        // comparando contraseña
+        const isMatch = await bcrypt.compare(password, UserFound.password)
+
+        if (!isMatch) return register.status(400).json({
+            message: "Incorrect Password"
+        })
+
+        // usando token
+        const token = await createAccessToken({ id: UserFound._id })
+
+        res.cookie('token', token)
+
+        res.json({
+            id: UserFound._id,
+            username: UserFound.username,
+            email: UserFound.email,
+            createdAT: UserFound.createdAt,
+            updatedAt: UserFound.updatedAt,
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+};
+
+export const logout = (req, res) => {
+    res.cookie("token", "", {
+        expires: new Date(0)
+    });
+    return res.sendStatus(200);
+}
+
+export const profile = async (req, res) => {
+
+    const userFound = await User.findById(req.user.id)
+
+    if(!userFound)  return res.status(400).json({message: "User not found"})
+
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAT: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    })
+}
---
+++ LOGIN/src/routes/auth.routes.js
@@ -0,0 +1,18 @@
+// Para crear un enrutador
+import { Router } from "express";
+import { login, register, logout, profile } from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
+
+// Objeto para crear multiples rutas (peticiosnes get, post...)
+
+const router = Router()
+
+router.post('/register', register)
+
+router.post('/login', login)
+
+router.post('/logout', logout)
+
+router.get('/profile', authRequired, profile)
+
+export default router
